428cee1540c3ec6eb0e6ab6e28f54986947eb4c3
bharath vallabaneni <42953917+8096998266@users.noreply.github.com>
2019-12-22 12:36:02 +0530

    Delete one.sh

--- one.sh
+++
@@ -1,9 +0,0 @@
-hi every one this is bharath
-i am learning git 
-i learnig devops course in javahome cloud
-welcome to devops trining
-my self bharath
-i should learn diffmerge
-i should learn properly instaling diffmerge
-what i want to learn
-revet and rebase
